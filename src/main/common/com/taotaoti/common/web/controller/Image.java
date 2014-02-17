2eda5a849cc87a36547be97eb90f7ebcd784332e
liulxiang <liulxiang@gmail.com>
2014-02-18 00:52:21 +0800

    提交help demo 网页

---
+++ src/main/common/com/taotaoti/common/web/controller/Image.java
@@ -0,0 +1,229 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2013 jdmr.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.taotaoti.common.web.controller;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+
+import com.taotaoti.common.generic.annotation.Column;
+
+/**
+ *
+ * @author jdmr
+ */
+@JsonIgnoreProperties({"id","thumbnailFilename","newFilename","contentType","dateCreated","lastUpdated"})
+public class Image implements Serializable {
+    private Long id;
+    private String name;
+    private String thumbnailFilename;
+    private String newFilename;
+    private String contentType;
+    private Long size;
+    private Date dateCreated;
+    private Date lastUpdated;
+    private String url;
+    private String thumbnailUrl;
+    private String deleteUrl;
+    private String deleteType;
+    
+    public Image() {}
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the thumbnailFilename
+     */
+    public String getThumbnailFilename() {
+        return thumbnailFilename;
+    }
+
+    /**
+     * @param thumbnailFilename the thumbnailFilename to set
+     */
+    public void setThumbnailFilename(String thumbnailFilename) {
+        this.thumbnailFilename = thumbnailFilename;
+    }
+
+    /**
+     * @return the newFilename
+     */
+    public String getNewFilename() {
+        return newFilename;
+    }
+
+    /**
+     * @param newFilename the newFilename to set
+     */
+    public void setNewFilename(String newFilename) {
+        this.newFilename = newFilename;
+    }
+
+    /**
+     * @return the contentType
+     */
+    public String getContentType() {
+        return contentType;
+    }
+
+    /**
+     * @param contentType the contentType to set
+     */
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    /**
+     * @return the size
+     */
+    public Long getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the dateCreated
+     */
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    /**
+     * @param dateCreated the dateCreated to set
+     */
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    /**
+     * @return the lastUpdated
+     */
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    /**
+     * @param lastUpdated the lastUpdated to set
+     */
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * @return the thumbnailUrl
+     */
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    /**
+     * @param thumbnailUrl the thumbnailUrl to set
+     */
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    /**
+     * @return the deleteUrl
+     */
+    public String getDeleteUrl() {
+        return deleteUrl;
+    }
+
+    /**
+     * @param deleteUrl the deleteUrl to set
+     */
+    public void setDeleteUrl(String deleteUrl) {
+        this.deleteUrl = deleteUrl;
+    }
+
+    /**
+     * @return the deleteType
+     */
+    public String getDeleteType() {
+        return deleteType;
+    }
+
+    /**
+     * @param deleteType the deleteType to set
+     */
+    public void setDeleteType(String deleteType) {
+        this.deleteType = deleteType;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" + "name=" + name + ", thumbnailFilename=" + thumbnailFilename + ", newFilename=" + newFilename + ", contentType=" + contentType + ", url=" + url + ", thumbnailUrl=" + thumbnailUrl + ", deleteUrl=" + deleteUrl + ", deleteType=" + deleteType + '}';
+    }
+
+}
---
+++ src/main/common/com/taotaoti/common/web/controller/QueryFileUploadController.java
@@ -0,0 +1,142 @@
+package com.taotaoti.common.web.controller;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.imgscalr.Scalr;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+@Controller
+@RequestMapping("/query")
+public class QueryFileUploadController {
+	 private static final Log log = LogFactory.getLog(QueryFileUploadController.class);
+	String fileUploadDirectory="/home/liulxiang/youyika/";
+	@RequestMapping(value = "/upload", method = RequestMethod.GET)
+	@ResponseBody
+    public Map list() {
+        List<Image> list =new ArrayList<Image>();
+        for(Image image : list) {
+            image.setUrl("/picture/"+image.getId());
+            image.setThumbnailUrl("/thumbnail/"+image.getId());
+            image.setDeleteUrl("/delete/"+image.getId());
+            image.setDeleteType("DELETE");
+        }
+        Map<String, Object> files = new HashMap<String, Object>();
+        files.put("files", list);
+        return files;
+    }
+    
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public  Map upload(MultipartHttpServletRequest request, HttpServletResponse response) {
+        log.debug("uploadPost called");
+        Iterator<String> itr = request.getFileNames();
+        MultipartFile mpf;
+        List<Image> list = new LinkedList<Image>();
+        
+        while (itr.hasNext()) {
+            mpf = request.getFile(itr.next());
+            
+            String newFilenameBase = UUID.randomUUID().toString();
+            String originalFileExtension = mpf.getOriginalFilename().substring(mpf.getOriginalFilename().lastIndexOf("."));
+            String newFilename = newFilenameBase + originalFileExtension;
+            String storageDirectory = fileUploadDirectory;
+            String contentType = mpf.getContentType();
+            
+            File newFile = new File(storageDirectory + "/" + newFilename);
+            try {
+                mpf.transferTo(newFile);
+                
+                BufferedImage thumbnail = Scalr.resize(ImageIO.read(newFile), 290);
+                String thumbnailFilename = newFilenameBase + "-thumbnail.png";
+                File thumbnailFile = new File(storageDirectory + "/" + thumbnailFilename);
+                ImageIO.write(thumbnail, "png", thumbnailFile);
+                
+                Image image = new Image();
+                image.setName(mpf.getOriginalFilename());
+                image.setThumbnailFilename(thumbnailFilename);
+                image.setNewFilename(newFilename);
+                image.setContentType(contentType);
+                image.setSize(mpf.getSize());
+                //image = imageDao.create(image);
+                
+                image.setUrl("/picture/"+image.getId());
+                image.setThumbnailUrl("/thumbnail/"+image.getId());
+                image.setDeleteUrl("/delete/"+image.getId());
+                image.setDeleteType("DELETE");
+                
+                list.add(image);
+                
+            } catch(IOException e) {
+            	
+            }
+            
+        
+        }
+        
+        Map<String, Object> files = new HashMap<String, Object>();
+        files.put("files", list);
+        return files;
+    }
+    
+    @RequestMapping(value = "/picture/{id}", method = RequestMethod.GET)
+    public void picture(HttpServletResponse response, @PathVariable Long id) {
+//        Image image = imageDao.get(id);
+//        File imageFile = new File(fileUploadDirectory+"/"+image.getNewFilename());
+//        response.setContentType(image.getContentType());
+//        response.setContentLength(image.getSize().intValue());
+//        try {
+//            InputStream is = new FileInputStream(imageFile);
+//            IOUtils.copy(is, response.getOutputStream());
+//        } catch(IOException e) {
+//            log.error("Could not show picture "+id, e);
+//        }
+    }
+    
+    @RequestMapping(value = "/thumbnail/{id}", method = RequestMethod.GET)
+    public void thumbnail(HttpServletResponse response, @PathVariable Long id) {
+//        Image image = imageDao.get(id);
+//        File imageFile = new File(fileUploadDirectory+"/"+image.getThumbnailFilename());
+//        response.setContentType(image.getContentType());
+//        response.setContentLength(image.getSize().intValue());
+//        try {
+//            InputStream is = new FileInputStream(imageFile);
+//            IOUtils.copy(is, response.getOutputStream());
+//        } catch(IOException e) {
+//            log.error("Could not show thumbnail "+id, e);
+//        }
+    }
+    
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public @ResponseBody List delete(@PathVariable Long id) {
+//        Image image = imageDao.get(id);
+//        File imageFile = new File(fileUploadDirectory+"/"+image.getNewFilename());
+//        imageFile.delete();
+//        File thumbnailFile = new File(fileUploadDirectory+"/"+image.getThumbnailFilename());
+//        thumbnailFile.delete();
+      //  imageDao.delete(image);
+        List<Map<String, Object>> results = new ArrayList<Map<String,Object>>();
+        Map<String, Object> success = new HashMap<String, Object>();
+        success.put("success", true);
+        results.add(success);
+        return results;
+    }
+}
